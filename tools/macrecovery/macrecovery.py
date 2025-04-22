4045c854d09c4e5df3de09529f6a0fa4fcee4610
Gabriel Luchina <luchina-gabriel@users.noreply.github.com>
2025-04-22 16:32:01 -0300

    Fix macrecovery (terminalsize)

--- tools/macrecovery/macrecovery.py
+++ tools/macrecovery/macrecovery.py
@@ -225,7 +225,7 @@ def save_image(url, sess, filename='', directory=''):
                 break
             fh.write(chunk)
             size += len(chunk)
-            terminalsize = max(os.get_terminal_size().columns - TERMINAL_MARGIN, 0)
+            terminalsize = 80
             if oldterminalsize != terminalsize:
                 print(f'\r{"":<{terminalsize}}', end='')
                 oldterminalsize = terminalsize
@@ -250,7 +250,7 @@ def verify_image(dmgpath, cnkpath):
 
     with open(dmgpath, 'rb') as dmgf:
         for cnkcount, (cnksize, cnkhash) in enumerate(verify_chunklist(cnkpath), 1):
-            terminalsize = max(os.get_terminal_size().columns - TERMINAL_MARGIN, 0)
+            terminalsize = 80
             print(f'\r{f"Chunk {cnkcount} ({cnksize} bytes)":<{terminalsize}}', end='')
             sys.stdout.flush()
             cnk = dmgf.read(cnksize)
